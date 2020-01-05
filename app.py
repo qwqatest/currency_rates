c7354870c2c3a75ca7afb8254146026d228461ce
SrSh <s.shtkov@gmail.com>
2020-01-05 17:47:02 +0200

    Add logic to show in coins how much rate is changed exactly

--- app.py
+++ app.py
@@ -1,50 +1,62 @@
 import requests
 import json
 import jsonpath
+from decimal import *
+from methods import *
 
-# https://hooks.slack.com/services/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
-
-webhook = 'https://hooks.slack.com/services/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'
-rates_url ='http://tech.money24.kharkov.ua/rates'
+webhook = ''
+rates_url = 'http://tech.money24.kharkov.ua/rates'
 
 payload = {
-	"appVersion": "1.1.0",
-	"deviceId": "2D454D85-4CE4-4C09-ABE8-4CBA1D29C6AA",
+    "appVersion": "1.1.0",
+    "deviceId": "2D454D85-4CE4-4C09-ABE8-4CBA1D29C6AA",
 }
 # Getting rates info
 response = requests.post(rates_url, data=json.dumps(payload))
-# print(response.content)
 
 # Getting US dollar rate from the response
 json_response = json.loads(response.text)
 current_us_rate = jsonpath.jsonpath(json_response, 'rates[11].buy')
-print(current_us_rate[0])
 
-# Changing rate type to float
+# Changing rate type to float for calculating purpose
 current_us_rate_float = float(current_us_rate[0])
-# print(type(current_us_rate_float))
 
-# Reading previous rate from tha file
-f = open("us_rate_history.txt","r")
+# Reading previous rate from the file
+f = open("us_rate_history.txt", "r")
 old_us_rate = f.read()
 
-# Changing rate type to float
+# Changing rate type to float for calculating purpose
 old_us_rate_float = float(old_us_rate)
-# print(type(old_us_rate_float))
-
-print("Старый курс: " + old_us_rate)
-# print(type(old_us_rate[0]))
-# print(type(current_us_rate))
 
 if current_us_rate_float == old_us_rate_float:
-	print("Nothing changed")
+    print("Nothing changed")
 else:
-	f = open("us_rate_history.txt","w+")
-	f.write(current_us_rate[0])
-	f.close()
-	if current_us_rate_float < old_us_rate_float:
-		data = {"text": "Курс доллара уменьшился: " + current_us_rate[0]}
-	else:
-		data = {"text": "Курс доллара увеличился: " + current_us_rate[0]}
+    f = open("us_rate_history.txt", "w+")
+    f.write(current_us_rate[0])
+    f.close()
+    if current_us_rate_float < old_us_rate_float:
+        # Make 2 number after the point
+        getcontext().prec = 2
+        # Calculating decreased difference
+        dec_difference = (Decimal(old_us_rate_float) - Decimal(current_us_rate_float)) * 100
 
-	resp = requests.post(webhook, data=json.dumps(data))
+        # Change type to "int" and format number to get rid off zero
+        dec_difference_int = int(dec_difference)
+        formatNumber(dec_difference_int)
+
+        data = {
+            "text": "Курс доллара уменьшился на " + str(dec_difference_int) + " коп. и составляет: " + current_us_rate[0]}
+    else:
+        # Make 2 number after the point
+        getcontext().prec = 2
+        # Calculating increased difference
+        inc_difference = (Decimal(current_us_rate_float) - Decimal(old_us_rate_float)) * 100
+
+        # Change type to "int" and format number to get rid off zero
+        inc_difference_int = int(inc_difference)
+        formatNumber(inc_difference_int)
+
+        data = {
+            "text": "Курс доллара увеличился на " + str(inc_difference_int) + " коп. и составляет: " + current_us_rate[0]}
+    # Send message to the Slack
+    resp = requests.post(webhook, data=json.dumps(data))
---
+++ methods.py
@@ -0,0 +1,6 @@
+
+def formatNumber(num):
+  if num % 1 == 0:
+    return int(num)
+  else:
+    return num
